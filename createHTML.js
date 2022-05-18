2844a3b7b6b15464d9fb8dfc34e88e16a21e75a9
Michael Hancock <shanehancock64@live.com>
2022-05-18 15:41:36 -0700

    Update createHTML.js

--- createHTML.js
+++ createHTML.js
@@ -1,6 +1,6 @@
 // Create employee cards
 
-const managerCard = function(manager) {
+const managerCard = function (manager) {
     return `
     <div class="col-4 mt-4">
         <div class="card h-100">
@@ -18,5 +18,39 @@ const managerCard = function(manager) {
     `;
 }
 
-const engineerCard = 
+const engineerCard = function (engineer) {
+    return  `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${engineer.name}</h3>
+                <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+const internCard = function (intern) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${intern.name}</h3>
+                <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school">School: ${intern.school}</p>
+            </div>
+    </div>
+</div>
+    `
+}
 
