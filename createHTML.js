0514c9121d1c3a5ba47ff5d375110ff20d2c551b
Michael Hancock <shanehancock64@live.com>
2022-05-18 16:34:44 -0700

    cant get html to generate cards
    🤬😭

--- createHTML.js
+++ createHTML.js
@@ -1,4 +1,7 @@
 const index = require('./index');
+const Manager = require('./lib/manager')
+const Engineer = require('./lib/engineer')
+const Intern = require('./lib/intern')
 // Create employee cards
 
 const managerCard = function (manager) {
@@ -56,30 +59,75 @@ const internCard = function (intern) {
 }
 
 createHTML = (data) => {
-    dataArray = []
+   pageArray = [];
+
+
     for(let i = 0; i < data.length; i++) {
         const employee = data[i];
         const role = employee.getRole();
     
     if (role === 'manager') {
         const createManager = managerCard(employee);
-        dataArray.push(createManager);
+        pageArray.push(createManager);
     }
 
     if (role === 'engineer') {
         const createEngineer = engineerCard(employee);
-        dataArray.push(createEngineer);
+        pageArray.push(createEngineer);
     }
 
     if (role === 'intern') {
         const createIntern = internCard(employee);
-        dataArray.push(createIntern);
+        pageArray.push(createIntern);
     }
  }
 
+
+ // joining strings 
+ const employeeCards = pageArray.join('')
+
+
+ const generateTeam = generateTeamPage(employeeCards); 
+ return generateTeam;
+
 }
 
-const employeeCards = dataArray.join('')
+const generateTeamPage = function (employeeCards) {   
+return`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Team Profile</title>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+   <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+   <link rel="stylesheet" href="./assets/style.css">
+</head>
+<body>
+   <header>
+       <nav class="navbar" id="navbar">
+           <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+       </nav>
+   </header>
+   <main>
+       <div class="container">
+           <div class="row justify-content-center" id="team-cards">
+               <!--Team Cards-->
+               ${employeeCards}
+           </div>
+       </div>
+   </main>
+   
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</html>
+`;
+}
 
 
-module.exports = createHTML;
+module.exports = createHTML; 
